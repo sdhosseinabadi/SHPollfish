00e983e4113644b5a98833549d40bc2b6f3d1e7f
sdhosseinabadi <107993175+sdhosseinabadi@users.noreply.github.com>
2023-11-13 21:32:38 +0330

    Update SHPollFishTests.swift

--- SHPollFishTests/SHPollFishTests.swift
+++ SHPollFishTests/SHPollFishTests.swift
@@ -19,6 +19,14 @@ class SHPollFishTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
    func testModel() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
